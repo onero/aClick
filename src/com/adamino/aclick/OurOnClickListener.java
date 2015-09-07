0679ba1ed3ddf26cbf1f1f2a930b99539db21f0e
adamino <adamlars90@gmail.com>
2015-09-07 23:49:28 +0200

    Project completed

---
+++ src/com/adamino/aclick/OurOnClickListener.java
@@ -0,0 +1,27 @@
+package com.adamino.aclick;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class OurOnClickListener implements OnClickListener{
+	
+	MainActivity caller;
+	private int count;
+
+	public OurOnClickListener(MainActivity activity) {
+		this.caller = activity;
+		this.count = 0;
+	}
+
+	@Override
+	public void onClick(View v) {
+		count = count +1;
+		String outputString = "The button got clicked " + count + " time";
+		if(count != 1) {
+			outputString += "s";
+		} 
+		caller.theTextView.setText(outputString);
+		
+	}
+
+}
